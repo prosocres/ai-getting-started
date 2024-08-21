1a3d248ba81ff2f5e7f8fd5842fcba71059f1fb4
prosocres <20094619+prosocres@users.noreply.github.com>
2024-08-21 06:35:06 -0600

    Starts langchain tutorial template

---
+++ src/scripts/indexRAGlocal.js
@@ -0,0 +1,47 @@
+import { ChatOpenAI } from "@langchain/openai";
+
+const llm = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  temperature: 0
+});
+
+import "cheerio";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
+import { pull } from "langchain/hub";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+
+const loader = new CheerioWebBaseLoader(
+  "https://lilianweng.github.io/posts/2023-06-23-agent/"
+);
+
+const docs = await loader.load();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+const splits = await textSplitter.splitDocuments(docs);
+const vectorStore = await MemoryVectorStore.fromDocuments(
+  splits,
+  new OpenAIEmbeddings()
+);
+
+// Retrieve and generate using the relevant snippets of the blog.
+const retriever = vectorStore.asRetriever();
+const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+
+const ragChain = await createStuffDocumentsChain({
+  llm,
+  prompt,
+  outputParser: new StringOutputParser(),
+});
+
+const retrievedDocs = await retriever.invoke("what is task decomposition");
+
